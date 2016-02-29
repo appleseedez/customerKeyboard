57d2c62c7e2569d771bcdad8148a50dc8131eb7c
NBkiller <administrator@administratordemac-mini.local>
2016-02-29 10:40:01 +0800

    Initial Commit

---
+++ customerKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customerKeyboard
+//
+//  Created by administrator on 16/2/29.
+//  Copyright © 2016年 NB_ZP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customerKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customerKeyboard
+//
+//  Created by administrator on 16/2/29.
+//  Copyright © 2016年 NB_ZP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
